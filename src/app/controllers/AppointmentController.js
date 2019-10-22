1921dacb6b15c1e570fb410252f9d8653ca1ef45
julioctba <40318136+julioctba@users.noreply.github.com>
2019-10-22 16:05:38 -0300

    Update Appoiment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,15 +1,18 @@
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
-import File from '../models/Appointment';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
     async index(req, res) {
+        const { page = 1 } = req.query;
         const appointments = await Appointment.findAll({
             where: { user_id: req.userId, canceled_at: null },
             order: ['date'],
             attributes: ['id', 'date'],
+            limit: 20,
+            offset: (page - 1) * 20,
             include: [
                 {
                     model: User,
